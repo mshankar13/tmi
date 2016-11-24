ec2718bdccb6708c96bfa773383e57766706776a
Isaac Ramos <isaacd.ramosny@gmail.com>
2016-11-24 16:20:17 -0500

    Update project requirements, added project setup instructions and some resources. Added Signup Form.

--- tmi.py
+++ tmi.py
@@ -1,13 +1,16 @@
-from flask import Flask, url_for, render_template
-
+from flask import Flask, url_for, render_template, request
+from model.SignUpForm import SignUpForm
 app = Flask(__name__)
-
+app.secret_key = 'dev-key'
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Welcome')
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('signup.html', title='Sign Up')
+    if request.method =='GET':
+        form = SignUpForm()
+        return render_template('signup.html', title='Sign Up', form=form)
 if __name__ == '__main__':
     app.run()
+
